da1dde2e7ed591df6a77e8a323e151f425bb295e
Omar Blanco <36765923+OmarssSeguridad@users.noreply.github.com>
2019-07-18 16:46:00 -0500

    Clases finalizadas

---
+++ app/src/main/java/com/example/mapainteractivo/EdificiosFragment.java
@@ -0,0 +1,25 @@
+package com.example.mapainteractivo;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EdificiosFragment extends Fragment {
+
+
+    public EdificiosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edificios, container, false);
+    }
+
+}
