17c314c71177b10f4869c2017dc91124b502087d
ADaanCruz <djdaan86@gmail.com>
2019-07-18 08:35:15 -0500

    Realtime Database Firebase.

--- app/src/main/java/com/example/mapainteractivo/MainActivity.java
+++ app/src/main/java/com/example/mapainteractivo/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.mapainteractivo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.mapainteractivo.firebase.FirebaseFirestore;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Firebase Connection";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference myRef = new FirebaseFirestore().getInstance();
+        myRef.setValue("¡Hola, mundo!");
+
+        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
     }
 }
---
+++ app/src/main/java/com/example/mapainteractivo/firebase/FirebaseFirestore.java
@@ -0,0 +1,20 @@
+package com.example.mapainteractivo.firebase;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseFirestore {
+
+    // Write a message to the database
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    public DatabaseReference getInstance() {
+        if (myRef == null) {
+            database = FirebaseDatabase.getInstance();
+            myRef = database.getReference("");
+        }
+        return myRef;
+    }
+
+}
