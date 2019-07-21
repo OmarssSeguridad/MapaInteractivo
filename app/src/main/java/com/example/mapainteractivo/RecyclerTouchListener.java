6c0d499ef4c243baee9aa26af59a9ebc6c849a94
ADaanCruz <djdaan86@gmail.com>
2019-07-20 23:58:58 -0500

    Create events in Firebase.

--- app/src/main/java/com/example/mapainteractivo/RecyclerTouchListener.java
+++ app/src/main/java/com/example/mapainteractivo/RecyclerTouchListener.java
@@ -24,7 +24,7 @@ public class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
             public void onLongPress(MotionEvent e) {
                 View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
                 if (child != null && clickListener != null) {
-                    clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    clickListener.onLongClick(child, recyclerView.getChildLayoutPosition(child));
                 }
             }
         });
